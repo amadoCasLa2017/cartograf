86b326fcb4c3bd966763cea261ecc0c0bb8e1716
alephcero <4lephcero@gmail.com>
2017-07-28 12:03:30 -0400

    un poco de limpieza

--- js/scripts.js
+++ js/scripts.js
@@ -17,35 +17,17 @@ function onEachFeature(feature, layer) {
 }
 
 
-//LEO DATA TENGO DOS FORMAS AJAX O AJAX LEAFLET PLUGIN NO FUNCA NINGUNA POR AHORA
-
-//https://stackoverflow.com/questions/20485723/add-external-geojson-to-leaflet-layer
-//https://github.com/calvinmetcalf/leaflet-ajax
-//var grafitis = new L.GeoJSON.AJAX("data.json");
-
-var grafitis = new L.geoJson();
-grafitis.addTo(map);
-
-$.ajax({
-dataType: "json",
-url: "data/data.geojson",
-success: function(data) {
-    $(data.features).each(function(key, data) {
-        grafitis.addData(data);
-    });
-}
-}).error(function() {});
-
-
-
 
 
 
 // leemos los datos de grafitis y ejecutamos onEachFeatire para
-// que le agrege a cada uno un popup 
+//  que le agrege a cada uno un popup 
  L.geoJSON(grafitis, {
- 	pointToLayer: function (feature, latlng) {
- 		return L.circleMarker(latlng, geojsonMarkerOptions);
- 	},
- 	onEachFeature: onEachFeature
- }).addTo(map);
+    pointToLayer: function (feature, latlng) {
+       return L.circleMarker(latlng, geojsonMarkerOptions);
+   },
+   onEachFeature: onEachFeature
+}).addTo(map);
+
+//probar mapbox
+//http://lyzidiamond.com/posts/external-geojson-mapbox
