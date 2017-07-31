c6057fac516c7897d2b7d418e731db38993ab0d1
alephcero <4lephcero@gmail.com>
2017-07-31 13:40:30 -0400

    lee data automaticamente del geojson

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,15 @@ var geojsonMarkerOptions = {
 	fillOpacity: 0.8
 };
 
-// funcion que se ejecuta cada vez que se lee uno de los grafitis.
-// muestra el pop up
-function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
+   var marker;
+   var grafitis;
 
 
-
-
-
-// leemos los datos de grafitis y ejecutamos onEachFeatire para
-//  que le agrege a cada uno un popup 
- L.geoJSON(grafitis, {
-    pointToLayer: function (feature, latlng) {
-       return L.circleMarker(latlng, geojsonMarkerOptions);
-   },
-   onEachFeature: onEachFeature
-}).addTo(map);
-
-//probar mapbox
-//http://lyzidiamond.com/posts/external-geojson-mapbox
+$.getJSON("../data/data.geojson",function(data){
+	grafitis = L.geoJson(data, {pointToLayer: function(feature, latlng){
+		//marker = L.marker(latlng);
+		marker = L.circleMarker(latlng, geojsonMarkerOptions);
+		marker.bindPopup(feature.properties.popupContent);
+		return marker
+	}
+}).addTo(map)})
